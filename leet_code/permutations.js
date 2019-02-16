dce67332e5f07db1475c839578357b2433d698f8
Sam Wellander <samwellander@gmail.com>
2019-02-16 10:53:56 -0500

    got it

---
+++ leet_code/permutations.js
@@ -0,0 +1,24 @@
+const input = [];
+const output = getPermutations(input);
+console.log(output);
+
+function getPermutations(array) {
+  if (!array.length) return [];
+  //keep track of all unique combinations
+  let permutations = [];
+  // define a function recurse
+  function recurse(array, history) {
+    //base case if array has single e add history.concat(array) to permutations
+    if (array.length < 2) return permutations.push(history.concat(array));
+    //for each e in array
+    array.forEach((e, i) => {
+      // recurse with the e as the start of the permutation (added to history) and removed from array
+      const arrCopy = array.slice("");
+      const base = arrCopy.splice(i, 1);
+      recurse(arrCopy, history.concat(base));
+    });
+  }
+
+  recurse(array, []);
+  return permutations;
+}
