a043176ed0b8f8fad5514262833f03dc9f3ffb80
Sam Wellander <samwellander@gmail.com>
2019-02-21 17:30:40 -0500

    30

---
+++ leet_code/pascalsTriangle.js
@@ -0,0 +1,17 @@
+function generate(n) {
+  if (n === 1) return [[1]];
+  const triangle = generate(n - 1);
+  const prevRow = triangle[n - 2];
+  const currRow = [];
+  let tl, tr;
+  for (let i = 0; i < n; ++i) {
+    tl = typeof prevRow[i - 1] === "number" ? prevRow[i - 1] : 0;
+    tr = typeof prevRow[i] === "number" ? prevRow[i] : 0;
+    currRow.push(tl + tr);
+  }
+  triangle.push(currRow);
+  return triangle;
+}
+
+const output = generate(7);
+console.table(output);
